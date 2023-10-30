e06266ecca295cf6cf6eaac2267c6bf563d5ff0d
JosiahFu <josiahfu@gmail.com>
2023-10-30 09:27:30 -0700

    Add magnetite blobs, needs testing

--- src/main/kotlin/archives/tater/netherarchives/WorldGen.kt
+++ src/main/kotlin/archives/tater/netherarchives/WorldGen.kt
@@ -14,8 +14,10 @@ import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier
 
 object WorldGen {
     private val MAGNETITE_DELTA = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier(NetherArchives.NAMESPACE, "magnetite_delta"))
+    private val MAGNETITE_BLOBS = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier(NetherArchives.NAMESPACE, "magnetite_blobs"))
 
     fun addFeatures() {
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.BASALT_DELTAS), GenerationStep.Feature.SURFACE_STRUCTURES, MAGNETITE_DELTA)
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.BASALT_DELTAS), GenerationStep.Feature.SURFACE_STRUCTURES, MAGNETITE_BLOBS)
     }
 }
