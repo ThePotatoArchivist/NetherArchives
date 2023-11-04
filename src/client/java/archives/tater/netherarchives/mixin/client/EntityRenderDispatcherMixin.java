6f9a37c47bfd7f45446ec2ffb6ac9ca29a5c8e35
JosiahFu <josiahfu@gmail.com>
2023-11-03 19:06:24 -0700

    Blazes render with blaze fire

---
+++ src/client/java/archives/tater/netherarchives/mixin/client/EntityRenderDispatcherMixin.java
@@ -0,0 +1,43 @@
+package archives.tater.netherarchives.mixin.client;
+
+import archives.tater.netherarchives.NetherArchives;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.BlazeEntity;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(EntityRenderDispatcher.class)
+public class EntityRenderDispatcherMixin {
+
+    @Unique
+    private static final SpriteIdentifier BLAZE_FIRE_0 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(NetherArchives.NAMESPACE, "block/blaze_fire_0"));
+    @Unique
+    private static final SpriteIdentifier BLAZE_FIRE_1 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(NetherArchives.NAMESPACE, "block/blaze_fire_1"));
+
+    @ModifyVariable(
+        method = "renderFire",
+        name = "sprite",
+            at = @At("STORE")
+    )
+    private Sprite setBlazeFireSprite0(Sprite originalSprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
+        return entity instanceof BlazeEntity ? BLAZE_FIRE_0.getSprite() : originalSprite;
+    }
+
+    @ModifyVariable(
+            method = "renderFire",
+            name = "sprite2",
+            at = @At("STORE")
+    )
+    private Sprite setBlazeFireSprite1(Sprite originalSprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
+        return entity instanceof BlazeEntity ? BLAZE_FIRE_1.getSprite() : originalSprite;
+    }
+}
