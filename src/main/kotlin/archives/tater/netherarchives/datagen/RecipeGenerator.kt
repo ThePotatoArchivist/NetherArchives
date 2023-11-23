c8af56cee5a0f7eb13effb5ff4ef1c3720591993
JosiahFu <josiahfu@gmail.com>
2023-11-22 20:17:17 -0800

    Added to recipe builder and fixed recipe advancements

--- src/main/kotlin/archives/tater/netherarchives/datagen/RecipeGenerator.kt
+++ src/main/kotlin/archives/tater/netherarchives/datagen/RecipeGenerator.kt
@@ -1,54 +1,59 @@
 package archives.tater.netherarchives.datagen
 
-import archives.tater.netherarchives.datagen.builder.crInput
-import archives.tater.netherarchives.datagen.builder.pattern
-import archives.tater.netherarchives.datagen.builder.shapedRecipe
+import archives.tater.netherarchives.datagen.builder.*
 import archives.tater.netherarchives.item.NetherArchivesItems
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.data.server.recipe.RecipeExporter
-import net.minecraft.data.server.recipe.RecipeProvider
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder
 import net.minecraft.item.Items
 import net.minecraft.recipe.book.RecipeCategory
 
 class RecipeGenerator(output: FabricDataOutput) : FabricRecipeProvider(output) {
-    // TODO Recipe Builder
     override fun generate(exporter: RecipeExporter) {
-        // TODO: These advancements try to trigger the minecraft: recipe for some reason?
-        RecipeProvider.offerSmelting(exporter, listOf(NetherArchivesItems.IRON_SLAG), RecipeCategory.MISC, Items.IRON_NUGGET, 0F, 25, null)
-        RecipeProvider.offerBlasting(exporter, listOf(NetherArchivesItems.IRON_SLAG), RecipeCategory.MISC, Items.IRON_NUGGET, 0F, 12, null)
+        exporter.recipes()
+    }
 
-//        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, Items.COMPASS)
+    private fun RecipeExporter.recipes() {
+        oreSmelting(RecipeCategory.MISC, NetherArchivesItems.IRON_SLAG, Items.IRON_NUGGET, 25, 0.08F)
 
-        exporter.shapedRecipe(RecipeCategory.TOOLS, Items.COMPASS) {
-            pattern(
-                " # ",
-                "#X#",
-                " # "
-            )
-            input('#', Items.IRON_INGOT)
-            crInput('X', NetherArchivesItems.MAGNETITE)
+        shaped(RecipeCategory.TOOLS, Items.COMPASS, recipeName = "compass_from_magnetite") {
+            patterns("""
+                 # 
+                #X#
+                 # 
+            """)
+            inputs {
+                '#' to Items.IRON_INGOT
+                'X' to NetherArchivesItems.MAGNETITE
+            }
+            itemCriterion(NetherArchivesItems.MAGNETITE)
         }
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, NetherArchivesItems.ROTTEN_FLESH_BLOCK)
-            .input(Items.ROTTEN_FLESH, 9)
-            .criterion(hasItem(Items.ROTTEN_FLESH), conditionsFromItem(Items.ROTTEN_FLESH))
-            .offerTo(exporter);
+        shapeless(RecipeCategory.MISC, NetherArchivesItems.ROTTEN_FLESH_BLOCK) {
+            inputs {
+                9 of Items.ROTTEN_FLESH
+            }
+            itemCriterion(Items.ROTTEN_FLESH)
+        }
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, NetherArchivesItems.BLAZE_DUST, 4)
-            .input(Items.BLAZE_POWDER)
-            .criterion(hasItem(Items.BLAZE_POWDER), conditionsFromItem(Items.BLAZE_POWDER))
-            .offerTo(exporter)
+        shapeless(RecipeCategory.COMBAT, NetherArchivesItems.BLAZE_DUST, 4) {
+            inputs {
+                +Items.BLAZE_POWDER
+            }
+            itemCriterion(Items.BLAZE_POWDER)
+        }
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, NetherArchivesItems.BLAZE_LANTERN)
-            .pattern("###")
-            .pattern("#X#")
-            .pattern("###")
-            .input('#', Items.NETHER_BRICK)
-            .input('X', Items.BLAZE_POWDER)
-            .criterion(hasItem(Items.BLAZE_POWDER), conditionsFromItem(Items.BLAZE_POWDER))
-            .offerTo(exporter)
+        shaped(RecipeCategory.COMBAT, NetherArchivesItems.BLAZE_LANTERN) {
+            patterns("""
+                ###
+                #X#
+                ###
+            """)
+            inputs {
+                '#' to Items.NETHER_BRICK
+                'X' to Items.BLAZE_POWDER
+            }
+            itemCriterion(Items.BLAZE_POWDER)
+        }
     }
 }
--- src/main/kotlin/archives/tater/netherarchives/datagen/builder/RecipeBuilder.kt
+++ src/main/kotlin/archives/tater/netherarchives/datagen/builder/RecipeBuilder.kt
@@ -1,17 +1,32 @@
 package archives.tater.netherarchives.datagen.builder
 
+import archives.tater.netherarchives.NetherArchives
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
+import net.minecraft.data.server.recipe.CookingRecipeJsonBuilder
 import net.minecraft.data.server.recipe.RecipeExporter
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder
+import net.minecraft.item.Item
 import net.minecraft.item.ItemConvertible
+import net.minecraft.recipe.AbstractCookingRecipe
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.RecipeSerializer
 import net.minecraft.recipe.book.RecipeCategory
+import net.minecraft.registry.Registries
+import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 
-inline fun RecipeExporter.shapedRecipe(category: RecipeCategory, outputItem: ItemConvertible, count: Int = 1, init: ShapedRecipeJsonBuilder.() -> Unit) {
-    ShapedRecipeJsonBuilder.create(category, outputItem).apply(init).offerTo(this)
+// CHANGE THIS when copying this file into a new class
+const val NAMESPACE = NetherArchives.NAMESPACE
+
+// Shaped
+
+inline fun RecipeExporter.shaped(category: RecipeCategory, outputItem: ItemConvertible, count: Int = 1, recipeName: String =  outputItem.asItem().id.path, init: ShapedRecipeJsonBuilder.() -> Unit) {
+    ShapedRecipeJsonBuilder.create(category, outputItem, count).apply(init).offerTo(this, Identifier(NAMESPACE, recipeName))
 }
 
-fun ShapedRecipeJsonBuilder.pattern(vararg rows: String) {
-    rows.forEach { pattern(it) }
+fun ShapedRecipeJsonBuilder.patterns(rows: String) {
+    rows.trimIndent().split('\n').forEach(::pattern)
 }
 
 fun ShapedRecipeJsonBuilder.itemCriterion(item: ItemConvertible) {
@@ -21,4 +36,101 @@ fun ShapedRecipeJsonBuilder.itemCriterion(item: ItemConvertible) {
 fun ShapedRecipeJsonBuilder.crInput(c: Char, item: ItemConvertible) {
     input(c, item)
     itemCriterion(item)
+}
+
+inline fun ShapedRecipeJsonBuilder.inputs(init: ShapedIngredientsBuilder.() -> Unit) {
+    ShapedIngredientsBuilder(this).init()
+}
+
+class ShapedIngredientsBuilder(private val recipeBuilder: ShapedRecipeJsonBuilder) {
+    infix fun Char.to(item: ItemConvertible) {
+        recipeBuilder.input(this, item)
+    }
+
+    infix fun Char.to(ingredient: Ingredient) {
+        recipeBuilder.input(this, ingredient)
+    }
+
+    infix fun Char.to(tagKey: TagKey<Item>) {
+        recipeBuilder.input(this, tagKey)
+    }
+}
+
+// Shapeless Recipe
+
+inline fun RecipeExporter.shapeless(category: RecipeCategory, outputItem: ItemConvertible, count: Int = 1, recipeName: String = outputItem.asItem().id.path, init: ShapelessRecipeJsonBuilder.() -> Unit) {
+    ShapelessRecipeJsonBuilder.create(category, outputItem, count).apply(init).offerTo(this, Identifier(NAMESPACE, recipeName))
+}
+
+fun ShapelessRecipeJsonBuilder.itemCriterion(item: ItemConvertible) {
+    criterion(FabricRecipeProvider.hasItem(item), FabricRecipeProvider.conditionsFromItem(item))
+}
+
+fun ShapelessRecipeJsonBuilder.crInput(item: ItemConvertible) {
+    input(item)
+    itemCriterion(item)
+}
+
+fun ShapelessRecipeJsonBuilder.inputs(init: ShapelessIngredientsBuilder.() -> Unit) {
+    ShapelessIngredientsBuilder(this).init()
+}
+
+class ShapelessIngredientsBuilder(private val recipeBuilder: ShapelessRecipeJsonBuilder) {
+    operator fun ItemConvertible.unaryPlus() {
+        recipeBuilder.input(this)
+    }
+
+    infix fun Int.of(item: ItemConvertible) {
+        recipeBuilder.input(item, this)
+    }
+
+    operator fun Ingredient.unaryPlus() {
+        recipeBuilder.input(this)
+    }
+
+    infix fun Int.of(ingredient: Ingredient) {
+        recipeBuilder.input(ingredient, this)
+    }
+
+    operator fun TagKey<Item>.unaryPlus() {
+        recipeBuilder.input(this)
+    }
+}
+
+// Cooking
+
+val ItemConvertible.id
+    get() = Registries.ITEM.getId(this as Item?)
+
+fun RecipeExporter.cookingRecipe(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, serializer: RecipeSerializer<out AbstractCookingRecipe>, method: String, cookingTime: Int = 100, experience: Float = 0F) {
+    CookingRecipeJsonBuilder.create(Ingredient.ofItems(inputItem), category, outputItem, experience, cookingTime, serializer)
+        .criterion(FabricRecipeProvider.hasItem(inputItem), FabricRecipeProvider.conditionsFromItem(inputItem))
+        .offerTo(this, Identifier(NAMESPACE, "${outputItem.id.path}_from_${method}_${inputItem.id.path}"))
+}
+
+fun RecipeExporter.smelting(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 100, experience: Float = 0F) {
+    cookingRecipe(category, inputItem, outputItem, RecipeSerializer.SMELTING, "smelting", cookingTime, experience)
+}
+
+fun RecipeExporter.smoking(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 50, experience: Float = 0F) {
+    cookingRecipe(category, inputItem, outputItem, RecipeSerializer.SMOKING, "smoking", cookingTime, experience)
+}
+
+fun RecipeExporter.blasting(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 50, experience: Float = 0F) {
+    cookingRecipe(category, inputItem, outputItem, RecipeSerializer.BLASTING, "blasting", cookingTime, experience)
+}
+
+fun RecipeExporter.campfire(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 300, experience: Float = 0F) {
+    cookingRecipe(category, inputItem, outputItem, RecipeSerializer.CAMPFIRE_COOKING, "campfire", cookingTime, experience)
+}
+
+fun RecipeExporter.oreSmelting(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 100, experience: Float = 0F) {
+    smelting(category, inputItem, outputItem, cookingTime, experience)
+    blasting(category, inputItem, outputItem, cookingTime / 2, experience)
+}
+
+fun RecipeExporter.foodCooking(category: RecipeCategory, inputItem: ItemConvertible, outputItem: ItemConvertible, cookingTime: Int = 100, experience: Float = 0F) {
+    smelting(category, inputItem, outputItem, cookingTime, experience)
+    smoking(category, inputItem, outputItem, cookingTime / 2, experience)
+    campfire(category, inputItem, outputItem, cookingTime * 3, experience)
 }
