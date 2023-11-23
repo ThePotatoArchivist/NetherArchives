94ce0ad4e8acfa7109816e6b92cd14bf4107d664
JosiahFu <josiahfu@gmail.com>
2023-11-22 20:22:38 -0800

    Fix Lodestone count

--- src/main/java/archives/tater/netherarchives/mixin/BlockItemMixin.java
+++ src/main/java/archives/tater/netherarchives/mixin/BlockItemMixin.java
@@ -1,15 +1,14 @@
 package archives.tater.netherarchives.mixin;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.item.*;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.stream.Stream;
@@ -19,7 +18,7 @@ public abstract class BlockItemMixin {
 	@Inject(at = @At("HEAD"), method = "onItemEntityDestroyed")
 	private void lodestoneDrop(ItemEntity entity, CallbackInfo ci) {
 		if (((BlockItem) (Object) this).getBlock() == Blocks.LODESTONE && entity.isOnFire()) {
-			ItemUsage.spawnItemContents(entity, Stream.of(new ItemStack(Items.NETHERITE_INGOT)));
+			ItemUsage.spawnItemContents(entity, Stream.of(new ItemStack(Items.NETHERITE_INGOT, entity.getStack().getCount())));
 		}
 	}
 }
